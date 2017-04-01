04761afdc024e35bf9a54ea1f840328bbb6b4453
afzal <afzal@SYSLP247>
2017-04-01 21:23:09 +0530

    sample project

---
+++ Angular2TypingApp/app/app.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component} from '@angular/core';
+@Component({
+    selector: 'my-app',
+    templateUrl:'View/home.html'
+})
+export class AppComponent {
+    constructor() {
+        this.name = 'Afzal'
+    }
+    name: string;
+    sayMyName() {
+        return this.name;
+    }
+}
